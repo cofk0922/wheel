15cb52a96df1c05515b79e98741354dc18e8dc4a
shishics17 <shishics17@gmail.com>
2014-03-08 13:09:40 +0000

    add Attribute for Performance Calculation

--- grails-app/domain/th/ac/chula/bsd/wheel/Car.groovy
+++ grails-app/domain/th/ac/chula/bsd/wheel/Car.groovy
@@ -1,7 +1,47 @@
 package th.ac.chula.bsd.wheel
 
-class Car {
+import java.util.Set;
 
-    static constraints = {
-    }
+class Car {
+	String band
+	String model
+	int year
+	String pcdCode
+	Float offSet
+	Float wheelSpace
+	Float weight
+	Float gearRatio
+
+//	Set wheelList = []
+	static hasMany = [wheelLists: CarWheelList]
+	
+	static constraints = {
+		band nullable: false
+		model nullable: false
+		pcdCode nullable: false
+	}
+	
+	public Car()
+	{
+		//Constructer
+	}
+	
+	public Car(String cBand, String cModel, int cYear, String cCode, Float cOffSet, Float cSpace, Float cWeight, Float cGearRatio)
+	{
+		this.band = cBand
+		this.model = cModel
+		this.year = cYear
+		this.pcdCode = cCode
+		this.offSet = cOffSet
+		this.wheelSpace = cSpace
+		this.weight = cWeight
+		this.gearRatio = cGearRatio
+		
+		//get all possible wheel by 
+	}
+	
+	public void CheckRate()
+	{
+		//TO DO Calculate score for all wheel avaliable for this car
+	}
 }
