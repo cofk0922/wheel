adf76d4e69d16651ba340a2c1abbc1db5b011fd2
bohtbirch <bohtbirch@gmail.com>
2014-03-09 15:12:49 +0000

    action for calendar

--- web-app/js/calendar/calendar.js
+++ web-app/js/calendar/calendar.js
@@ -14,19 +14,29 @@
 			allDaySlot: false,
 			events: data,
 			eventDrop: function(event,dayDelta,minuteDelta,allDay,revertFunc) {
-				$('#status').text(
-					event.title + " was moved " +
-					dayDelta + " days and " +
-					minuteDelta + " minutes."
-				);
+				$.post('../appointment/validateTime', { 'id': event.id,'dayDelta':dayDelta,'minuteDelta':minuteDelta,'act': 'update' }, function(result){
+					if (result.isValid){
+						if (!confirm("Are you sure about this change?")) {
+							revertFunc();
+						}else{
+							$.post( "../appointment/editEvent", { 'id':  event.id, 'dayDelta':dayDelta,'minuteDelta':minuteDelta,'act': 'update'});
+							$('#status').text(
+							event.title + " was moved " +
+							dayDelta + " days and " +
+							minuteDelta + " minutes."
+							);
+						}
+					}else{
+						alert("Event can't change for this time");
+						revertFunc();
+					}
+				});				
 			},
 			eventClick: function(event, element) {
-				var title = prompt('App');
-				if (title) {
-					event.title = title;
+				if (confirm("Are you sure to remove this event?")) {
+					$.post( "../appointment/editEvent", { 'id':  event.id,'act': 'remove'});
+					$('#calendar').fullCalendar( 'removeEvents' , event.id);
 				}
-				$('#calendar').fullCalendar('updateEvent', event);
-
 			},
 			selectable: true,
 			select: function(start,end) {
@@ -35,24 +45,27 @@
 					$('#calendar').fullCalendar('changeView', 'agendaDay' );
 					$('#calendar').fullCalendar('gotoDate', start.getFullYear(), start.getMonth(), start.getDate());
 				} else {
-					$.post('../appointment/validateTime', { 'startdate': start }, function(result) {
+					$.post('../appointment/validateTime', { 'startdate': start,'act': 'add' }, function(result) {
 						if (result.isValid){
 							var title = prompt('Appointment');
 							if (title) {
-								$('#calendar').fullCalendar('renderEvent',
-									{
-										title: title,
-										start: start,
-										end: end,
-										allDay: false
-									},
+								$.post( "../appointment/editEvent", { 'title':  title, 'startdate': start, 'act': 'add'}, function(result){
+									console.log(result);
+									$('#calendar').fullCalendar('renderEvent',
+										{
+											id: result.id,
+											title: result.title,
+											start: result.start,
+											end:	end,
+											allDay: result.allDay
+										},
 									$('#status').text(title + " was added "),
 									true // make the event "stick"
 									
-								);
-								$.post( "../appointment/saveDate", { 'title':  title, 'startdate': start});
-						}
-						$('#calendar').fullCalendar('unselect')
+									);
+									$('#calendar').fullCalendar('unselect')
+								});
+							}
 						}
 					})
 				}
