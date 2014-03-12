3629f804e4da72e13c35368f1b025d5f67d6f37a
bohtbirch@gmail.com <bohtbirch@gmail.com@fbd5478f-6c4a-f01b-112b-36ce64456432>
2014-03-12 08:46:41 +0000

    return Datasource to connect MYSQL

--- trunk/grails-app/conf/DataSource.groovy
+++ trunk/grails-app/conf/DataSource.groovy
@@ -1,9 +1,9 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-   username = "sa"
-    password = ""	
-//	driverClassName = "com.mysql.jdbc.Driver"
+//    driverClassName = "org.h2.Driver"
+//    username = "sa"
+//    password = ""	
+	driverClassName = "com.mysql.jdbc.Driver"
 	dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
 	
 	
@@ -20,8 +20,8 @@ environments {
     development {
         dataSource {
             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-           url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-//			url = "jdbc:mysql://localhost/softwareStudio2?useUnicode=yes&characterEncoding=UTF-8"
+//           url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+			url = "jdbc:mysql://localhost/softwareStudio2?useUnicode=yes&characterEncoding=UTF-8"
 			username = "root"
 			password = "1234"
        }
