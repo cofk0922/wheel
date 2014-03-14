fde8aaaacc96167903405a977ecfba149d0ecfa0
mvy_27 <mvy_27@hotmail.com>
2014-03-14 16:02:19 +0000

    test unit inventory

---
+++ test/unit/th/ac/chula/bsd/inventory/ProductPurchaseLineSpec.groovy
@@ -0,0 +1,21 @@
+package th.ac.chula.bsd.inventory
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+import th.ac.chula.bsd.inventory.ProductPurchaseLine;
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(ProductPurchaseLine)
+class ProductPurchaseLineSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
