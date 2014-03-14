cf983a445794c6c8483082937042ec1c08e4215c
mvy_27@hotmail.com <mvy_27@hotmail.com@fbd5478f-6c4a-f01b-112b-36ce64456432>
2014-03-14 16:02:19 +0000

    test unit inventory

---
+++ trunk/test/unit/th/ac/chula/bsd/inventory/PreProductTransferLineSpec.groovy
@@ -0,0 +1,21 @@
+package th.ac.chula.bsd.inventory
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+import th.ac.chula.bsd.inventory.PreProductTransferLine;
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(PreProductTransferLine)
+class PreProductTransferLineSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
---
+++ trunk/test/unit/th/ac/chula/bsd/inventory/ReceivePurchaseSpec.groovy
@@ -0,0 +1,20 @@
+package th.ac.chula.bsd.inventory
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(ReceivePurchase)
+class ReceivePurchaseSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
