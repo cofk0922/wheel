f4d96fcec30d1b68d12eefa3e9214cd237673219
shishics17@gmail.com <shishics17@gmail.com@fbd5478f-6c4a-f01b-112b-36ce64456432>
2014-03-15 14:33:27 +0000

    change Car -> CarColor

--- trunk/grails-app/domain/th/ac/chula/bsd/wheel/CarModel.groovy
+++ trunk/grails-app/domain/th/ac/chula/bsd/wheel/CarModel.groovy
@@ -4,7 +4,7 @@ import java.util.Set;
 
 class CarModel {
 	
-	String model
+	String modelName
 	int year
 	String pcdCode
 	Float offSet
@@ -13,31 +13,8 @@ class CarModel {
 	//MaxWheel defaultWheel
 	int defaultTireSize
 	
-	// Position Wheel front 
-	Integer frontX1
-	Integer frontY1
-	Integer frontX2
-	Integer frontY2
-	Integer frontWidth
-	Integer frontHeight
-		
-	//Position Wheel back	
-	Integer backX1
-	Integer backY1
-	Integer backX2
-	Integer backY
-	Integer backWidth
-	Integer backHeight
-	
-	String  hVal
-	String  sVal
-	String  vVal
-	String  hexVal
-	
-	String carImage
-	
 //	Set wheelList = []
-	static hasMany = [wheelLists: CarWheelList]
+	static hasMany = [wheelLists: CarWheelList, carColor: CarColor]
 	
 	static hasOne = [band: CarBand]
 	
@@ -54,4 +31,5 @@ class CarModel {
 	{
 		//TO DO Calculate score for all wheel avaliable for this car
 	}
+
 }
