108c2abb8f2cdc75a7cee22057b754dffcd07722
agoya1133@gmail.com <agoya1133@gmail.com@fbd5478f-6c4a-f01b-112b-36ce64456432>
2014-03-21 13:59:35 +0000

    req

--- trunk/grails-app/domain/th/ac/chula/bsd/wheel/RequisitionLine.groovy
+++ trunk/grails-app/domain/th/ac/chula/bsd/wheel/RequisitionLine.groovy
@@ -34,6 +34,33 @@ class RequisitionLine {
 		this.product = prod
 		this.amount = amt
 	}
+	
+	public void newRequisit(){
+		this.status = RequisitionLineStatus.NEW 
+	}
+	
+	public void readyRequisit(){
+		this.status = RequisitionLineStatus.READY_REQUISIT
+		branch.decreaseProductStock(product,amount)
+	  
+	}
+	 
+ 	public Boolean checkProductStock(){
+		Boolean result = false
+		def prod = product.getProductStock(branch)	 
+		if(prod > amount){
+		   result = true
+		}	 
+		return result
+	}
+	
+	public void requisited(){
+		this.status = RequisitionLineStatus.REQUISITED
+	}
+	
+	public void cancelRequisit(){
+		this.status = RequisitionLineStatus.CANCEL
+	}
 }
 
 enum RequisitionLineStatus {
